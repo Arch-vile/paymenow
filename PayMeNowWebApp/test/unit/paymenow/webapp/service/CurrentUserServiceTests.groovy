4cf8498dc9e1b3f4dd233e11439f5b7ffc13efdd
raipe <raipe.trashmail@gmail.com>
2013-05-24 07:38:45 +0300

    Adding tests

--- PayMeNowWebApp/test/unit/paymenow/webapp/service/CurrentUserServiceTests.groovy
+++ PayMeNowWebApp/test/unit/paymenow/webapp/service/CurrentUserServiceTests.groovy
@@ -4,14 +4,37 @@ package paymenow.webapp.service
 
 import grails.test.mixin.*
 import org.junit.*
+import paymenow.webapp.domain.EmailAccount
+import paymenow.webapp.domain.User
+import grails.test.mixin.domain.DomainClassUnitTestMixin
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(CurrentUserService)
+@Mock([UserManagementService, User, EmailAccount])
+@TestMixin(DomainClassUnitTestMixin)
 class CurrentUserServiceTests {
 
-    void testSomething() {
-        fail "Implement me"
+    void testGetUser() {
+		
     }
+	
+	void testGetUserMasterEmailFromMultipleFirst(){
+		/*mockDomain(EmailAccount)
+		mockDomain(User, [ 
+			[login: "mikko", 
+				emails: [
+					[isMaster: true, email: "email1@emails.com"]
+					]
+			]
+		])
+		
+		def looseControl = mockFor(UserManagementService)
+		looseControl.demand.getUser { User.get(0) }
+		service.userManagementService = looseControl.createMock()
+		assert service.getUserMasterEmail().email == "email1@emails.com"
+		*/		
+	}
+	
 }
